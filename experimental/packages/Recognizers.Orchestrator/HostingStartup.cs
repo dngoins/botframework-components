19854bb89380f6d5c70c265faa682b919df0e46f
Peter Innes <peinnes@microsoft.com>
2021-01-27 15:03:41 -0800

    Establishing experimental/packages directory per Ryan's repo structure. Adding Microsoft.Bot.Components.sln with single project, Microsoft.Bot.Components.Recognizers.Orchestrator for registering recognizer with adaptive runtime. Including preview NuGet package for testing purposes.

---
+++ experimental/packages/Recognizers.Orchestrator/HostingStartup.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.AI.Orchestrator;
+
+[assembly: HostingStartup(typeof(Microsoft.Bot.Components.Recognizers.Orchestrator.HostingStartup))]
+
+namespace Microsoft.Bot.Components.Recognizers.Orchestrator
+{
+    public class HostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            ComponentRegistration.Add(new OrchestratorComponentRegistration());
+        }
+    }
+}
